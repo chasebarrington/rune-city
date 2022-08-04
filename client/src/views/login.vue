e6d0cf229fdd7d91d5a2f9cc8c41c37af714d8b5
iMythik <iMythik@gmail.com>
2022-08-04 19:35:02 -0400

    chatbox test
    added some more lil chat stuff

--- client/src/views/login.vue
+++ client/src/views/login.vue
@@ -28,13 +28,19 @@ export default {
 </script>
 
 <template>
-  <div v-for="(message, index) in messages" :key="index">
-    {{message}}
-  </div>
-  <div>
-    <input v-model="message">
-    <Button @click="send" color="emerald">
-      send
-    </Button>
+  <div class="bg-gray-800 w-screen h-screen">
+    <div class="bg-gray-900 rounded-md px-2 py-2 w-fit h-screen grid place-content-end ml-auto">
+      <div class="overflow-y-auto">
+        <div class="text-gray-100 my-2" v-for="(message, index) in messages" :key="index">
+          {{message}}
+        </div>
+      </div>
+      <div class="mt-4">
+        <input class="appearance-none rounded-md px-2 py-1 mr-2" v-model="message">
+        <Button @click="send" color="emerald">
+          send
+        </Button>
+      </div>
+    </div>
   </div>
 </template>
